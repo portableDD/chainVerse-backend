5d1dd519c1f341d5acd6d758d5cfc723ccc64d15
clintjeff2 <clintjeff2@gmail.com>
2025-04-17 11:58:51 +0100

    Added routes for updating profile, creating profile, uploading profile image, updating password, and verifying the email given that the email verification in the auth controller is specific to the Student model

--- src/routes/organization.js
+++ src/routes/organization.js
@@ -1,46 +1,73 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const auth = require("../middlewares/auth");
-const organizationController = require("../controllers/organizationController");
+const auth = require('../middlewares/auth');
+const organizationController = require('../controllers/organizationController');
+
+const { uploadProfileImage } = require('../config/multerConfig');
+const userOrganizationController = require('../controllers/userOrganizationController');
 
 // @route POST /organization/subscribe
 // @desc Allows an organization to purchase a subscription based on available plans.
-router.post(
-  "/organization/subscribe",
-  auth,
-  organizationController.subscribeToPlan
-);
+router.post('/subscribe', auth, organizationController.subscribeToPlan);
 
 // @route GET /organization/{organizationId}/subscription
 // @desc Fetches details about the organization's current subscription
 router.get(
-  "/organization/:id/subscription",
-  auth,
-  organizationController.getSubscriptionPlan
+	'/:id/subscription',
+	auth,
+	organizationController.getSubscriptionPlan
 );
 
 // @route PUT /organization/{organizationId}/subscription/{newSubscriptionPlanId}/update
 // @desc Allows organizations to switch to a different plan, adjusting their member limit accordingly.
 router.put(
-  "/organization/:id/subscription/:planId/update",
-  auth,
-  organizationController.updateSubscriptionPlan
+	'/:id/subscription/:planId/update',
+	auth,
+	organizationController.updateSubscriptionPlan
 );
 
 // @route DELETE /organization/{organizationId}/subscription/cancel
 // @desc Organizations can cancel their subscription, which will restrict further member additions.
 router.delete(
-  "/organization/:id/subscription/cancel",
-  auth,
-  organizationController.cancelSubscriptionPlan
+	'/:id/subscription/cancel',
+	auth,
+	organizationController.cancelSubscriptionPlan
 );
 
 // @route GET /organization/{organizationId}/subscription/invoice
 // @desc Generates an invoice/receipt for past payments.
 router.get(
-  "/organization/:id/subscription/invoice",
-  auth,
-  organizationController.getSubscriptionPlanInvoice
+	'/:id/subscription/invoice',
+	auth,
+	organizationController.getSubscriptionPlanInvoice
 );
+
+// Profile management routes
+router
+	.route('/profile')
+	.get(auth, userOrganizationController.getProfile)
+	.post(userOrganizationController.registerUser);
+
+router.put('/profile/update', auth, userOrganizationController.updateProfile);
+
+router.get(
+	'/profile/verify-email',
+	auth,
+	userOrganizationController.verifyEmail
+);
+
+router.put(
+	'/profile/change-password',
+	auth,
+	userOrganizationController.changePassword
+);
+router.post(
+	'/profile/upload-image',
+	auth,
+	uploadProfileImage.single('profileImage'),
+	userOrganizationController.uploadProfileImage
+);
+
+module.exports = router;
 
 module.exports = router;
