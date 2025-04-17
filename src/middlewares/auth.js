f85af0cd4bcde314db0a7593b72c46284301e8b8
clintjeff2 <clintjeff2@gmail.com>
2025-04-17 11:48:43 +0100

    Fixed auth middleware to take token from secure Bearer

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -2,22 +2,23 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 module.exports = (req, res, next) => {
-  // Get token from header
-  const token = req.header('x-auth-token');
+	// Get token from Authorization header
+	const authHeader = req.header('Authorization');
 
-  // Check if no token
-  if (!token) {
-    return res.status(401).json({ msg: 'No token, authorization denied' });
-  }
+	if (!authHeader || !authHeader.startsWith('Bearer ')) {
+		return res.status(401).json({ msg: 'No token, authorization denied' });
+	}
 
-  // Verify token
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded.user;
-    next();
-  } catch (err) {
-    res.status(401).json({ msg: 'Token is not valid' });
-  }
+	const token = authHeader.split(' ')[1];
+
+	// Verify token
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = decoded;
+		next();
+	} catch (err) {
+		res.status(401).json({ msg: 'Token is not valid' });
+	}
 };
 
 // const jwt = require("jsonwebtoken")
@@ -55,4 +56,3 @@ module.exports = (req, res, next) => {
 //     return res.status(401).json({ error: "Authentication failed" })
 //   }
 // }
-
