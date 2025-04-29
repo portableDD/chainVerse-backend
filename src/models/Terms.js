9fb8331fc1ae9e79499176b3aa6b440084f12612
Sani Abubakar Adam <Athernee@gmail.com>
2025-04-29 22:42:29 +0100

    feat: add Terms schema for Terms and Conditions management

---
+++ src/models/Terms.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const termsSchema = new mongoose(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    content: {
+      type: String,
+      required: true,
+    }
+  },
+  {
+    timestamps: true // Automaticall adds createdAt and updatedAt
+  }
+);
+
+module.exports = mongoose.model('Terms', termsSchema);
